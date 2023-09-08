c5487478a2e9d0d815944e6ce002a1574c23dd83
webdevlogics <webdevlogics@pop-os.localdomain>
2023-09-08 11:30:03 -0700

    added books page

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,13 +1,19 @@
 import goodverse from '~/assets/goodverse.png'
-import new_release from '~/assets/new_release.webp'
 import main_background from '~/assets/main_background_jane.webp'
-import haunting_good_times from '~/assets/haunting_good_times.webp'
 import author_jane from '~/assets/author_jane.webp'
+import book_good_1 from '~/assets/book_good_1.webp'
+import book_good_2 from '~/assets/book_good_2.webp'
+import book_good_3 from '~/assets/book_good_3.webp'
+import book_haunted_1 from '~/assets/book_haunted_1.webp'
+import book_haunted_2 from '~/assets/book_haunted_2.webp'
 
 export {
   goodverse,
-  new_release,
   main_background,
-  haunting_good_times,
   author_jane,
+  book_good_1,
+  book_good_2,
+  book_good_3,
+  book_haunted_1,
+  book_haunted_2,
 }
