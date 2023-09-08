5d0ad6812c03714b86d07d4bbf014ffafa1092d8
webdevlogics <webdevlogics@pop-os.localdomain>
2023-09-07 20:40:40 -0700

    added events page

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,5 +2,12 @@ import goodverse from '~/assets/goodverse.png'
 import new_release from '~/assets/new_release.webp'
 import main_background from '~/assets/main_background_jane.webp'
 import haunting_good_times from '~/assets/haunting_good_times.webp'
+import author_jane from '~/assets/author_jane.webp'
 
-export { goodverse, new_release, main_background, haunting_good_times }
+export {
+  goodverse,
+  new_release,
+  main_background,
+  haunting_good_times,
+  author_jane,
+}
